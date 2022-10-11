fb8417ea1501cf7b867521dc68c0dd81bdb5fc4f
M H <mh@localhost>
2022-10-11 17:21:24 +0500

    login register apis

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('/logout', function (Request $request) {
+    $request->user()->currentAccessToken()->delete();
+});
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->validate([
+        'email'=> 'required|email',
+        'password'=> 'required'
+    ]);
+
+    if(auth()->once($credentials)){
+        $token = auth()->user()->createToken($request->email);
+ 
+        return ['token' => $token->plainTextToken];
+    }else{
+        return ['message' => "Not logged in."];
+    }
+
+});
+
+Route::post('/register', function (Request $request) {
+    $data = $request->validate([
+        'name'=> 'required|min:4',
+        'email'=> 'required|email|unique:users,email',
+        'password'=> 'required|min:4|max:12'
+    ]);
+    $data['password'] = bcrypt($data['password']);
+
+    $user = new User;
+    $user->fill($data);
+    $user->save();
+
+    $token = $user()->createToken($request->email);
+    return ['token' => $token->plainTextToken];
+
 });
