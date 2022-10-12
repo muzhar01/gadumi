14313ab01cd3838b3a1e3350fd63284b1200bbae
Khizar wahab <khizarwahab19@gmail.com>
2022-10-12 11:24:01 +0500

    add migrations

---
+++ database/migrations/2022_10_12_060537_create_plans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->double('price');
+            $table->string('currency_iso');
+            $table->string('currency_symbol')->nullable();
+            $table->boolean('currency_left')->default(true);
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+};
