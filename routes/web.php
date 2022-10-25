a08488688e017bc0893f4dcbd6d948c255df1eb2
wadood <wanikhan919@gmail.com>
2022-10-25 13:07:49 +0500

    exercise detail page done

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/portal/{params?}', 'portal');
+Route::view('/portal/{params?}{params1?}/{params2?}/{params3?}/{params4?}', 'portal');
 
 Route::get('/admin',[AdminController::class,'index'])->name('admin.login');
 Route::post('/admin/login',[AdminController::class,'login'])->name('admin.login.submit');
