f647198dc48126763014047a2d6d94b111fede5e
Khizar wahab <khizarwahab19@gmail.com>
2022-12-22 17:43:23 +0500

    Changes

--- resources/js/components/Menu.jsx
+++ resources/js/components/Menu.jsx
@@ -11,19 +11,19 @@ function Menu() {
               </Link>
             </li>
             <li>
-              <Link to="/portal/setting" className="settings">
+              <Link to="#" className="prime_account">
                 <img src="/images/prime_account.svg" width="40" height="40" alt=""/>
                 <span>Premium account</span>
               </Link>
             </li>
             <li>
-              <Link to="/portal/setting" className="settings">
+              <Link to="#" className="contact_us">
                 <img src="/images/contact_us.svg" width="40" height="40" alt=""/>
                 <span>contact us</span>
               </Link>
             </li>
             <li>
-              <Link to="/logout" className="settings">
+              <Link to="/logout" className="logout">
                 <img src="/images/logout.svg" width="40" height="40" alt=""/>
                 <span>log out</span>
               </Link>
@@ -34,20 +34,3 @@ function Menu() {
 }
 
 export default Menu;
-
-/*<li className="list-group-item">
-              <img src="/images/settings.svg" alt="" srcSet=""/>
-              <Link to="/portal/setting" className="settings">ustawienia</Link>
-            </li>
-            <li className="list-group-item">
-              <img src="/images/prime_account.svg" alt="" srcSet=""/>
-              <a href="#" className="prime_account">konto premium</a>
-            </li>
-            <li className="list-group-item">
-              <img src="/images/contact_us.svg" alt="" srcSet=""/>
-              <a href="#" className="contact_us">skontaktuj się z nami</a>
-            </li>
-            <li className="list-group-item">
-              <img src="/images/logout.svg" alt="" srcSet=""/>
-              <Link to="/logout" className="logout">wyloguj się</Link>
-          </li>*/
